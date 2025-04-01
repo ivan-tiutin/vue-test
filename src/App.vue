2d7257bcfc61448404ab2ddd197b24c59409048f
Ivan Tiutin <tyutin.iv95@gmail.com>
2025-04-01 11:02:50 +0500

    Updated test

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,27 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    data() {
+        return {
+            name: 'John',
+            status: 'pending',
+            tasks: ['task1', 'task2', 'task3', 'task4'],
+        }
+    },
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <h1>{{ name }}</h1>
+    <p v-if="status === 'active'">User is active</p>
+    <p v-else-if="status === 'pending'">User is pending</p>
+    <p v-else>User in inactive</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <h1>Tasks:</h1>
+    <ul>
+        <li v-for="task in tasks" :key="task">{{ task }}</li>
+    </ul>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
