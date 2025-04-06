61df8c7fbf55667a4cbd4d7bb71ab0a486de43d3
Ivan Tiutin <tyutin.iv95@gmail.com>
2025-04-06 21:43:27 +0500

    Started adding components

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import NavbarComponent from '@/components/NavbarComponent.vue';
+import HeroComponent from '@/components/HeroComponent.vue';
+import HomeCards from '@/components/HomeCards.vue';
+</script>
+
+<template>
+    <NavbarComponent />
+    <HeroComponent />
+    <HomeCards />
+</template>
